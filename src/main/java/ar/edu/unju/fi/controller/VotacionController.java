e153a3eda0258361514c7abe9b6a6fd6e828e5dc
Jonatan Garcia <sacha.255555@gmail.com>
2022-05-28 17:09:59 -0300

    Agregados algunos estilos y votacion.html

---
+++ src/main/java/ar/edu/unju/fi/controller/VotacionController.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VotacionController {
+	@GetMapping("/votacion")
+	public String getIndexPage(Model model) {
+		
+		return "votacion"; 
+	}
+
+}
